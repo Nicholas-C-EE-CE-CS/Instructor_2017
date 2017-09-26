3cd30602bdd2ea4195e1ed4487cb143cf45d38bd
JochenFischer <jochen.elon@gmail.com>
2017-09-25 22:41:49 -0400

    Expanded HW class, encoders
    Robot is using encoders now to set speed and define the distance
    traveled.

--- TeamCode/src/main/java/edu/elon/robotics/HardwareDriveBot.java
+++ TeamCode/src/main/java/edu/elon/robotics/HardwareDriveBot.java
@@ -11,9 +11,11 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
  *   - motorRight
  *
  * @author Jochen Fischer
+ * @version 2 - 2017-09-25 as shown in class with some additional comments
  */
 
 public class HardwareDriveBot {
+
     //----------------------------------------------------------
     // public member variables representing parts of the robot
     //----------------------------------------------------------
@@ -22,19 +24,30 @@ public class HardwareDriveBot {
     public DcMotor leftMotor = null;
     public DcMotor rightMotor = null;
 
+    // hardware specific constants:
+    public static final double ENCODER_ROTATION_40 = 1120; // encoder ticks per rotation, 40:1 motor
+    public static final double ENCODER_ROTATION_60 = 1680; // encoder ticks per rotation, 60:1 motor
+    public static final double WHEEL_DIAMETER = 4.0;       // inches
+    public static final double TRACK = 10.52;              // inches
+    public static final double WHEEL_BASE = 8.5;           // inches
+
+    // other useful constants:
+    // TODO: add on Wednesday
+    // STOP
+    // SLOW_POWER
+    // NORMAL_POWER
+
     //----------------------------------------------------------
     // local member variables
     //----------------------------------------------------------
     private HardwareMap hwMap = null;
 
     //----------------------------------------------------------
-    // functions that help opereate the robot
+    // functions that help operate the robot
     //----------------------------------------------------------
 
     // Constructor does nothing
-    public HardwareDriveBot() {
-
-    }
+    public HardwareDriveBot() {}
 
     /**
      * Initialize the robot.
@@ -50,7 +63,35 @@ public class HardwareDriveBot {
         leftMotor  = hwMap.get(DcMotor.class, "leftMotor");
         rightMotor = hwMap.get(DcMotor.class, "rightMotor");
 
+        // one of the motors needs to be reversed since they are mounted in opposite directions:
         leftMotor.setDirection(DcMotor.Direction.REVERSE);
         rightMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        // reset and use encoders:
+        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    /**
+     * start, stop, spin - convenience function to run and stop the robot
+     *
+     * @author Jochen Fischer
+     */
+    void stop() {
+        leftMotor.setPower(0.0);
+        rightMotor.setPower(0.0);
+    }
+
+    void start(double power) {
+        leftMotor.setPower(power);
+        rightMotor.setPower(power);
+    }
+
+    void spin(double power) {
+        leftMotor.setPower(-power);
+        rightMotor.setPower(power);
     }
 }
---
+++ TeamCode/src/main/java/edu/elon/robotics/MoveWithEncoder.java
@@ -0,0 +1,103 @@
+package edu.elon.robotics;
+
+import android.util.Log;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Moves the robot with encoders.
+ *
+ * @author Jochen Fischer
+ * @version 1
+ */
+
+@Autonomous(name="Move With Encoders V1", group="Elon")
+//@Disabled
+public class MoveWithEncoder extends LinearOpMode {
+
+    // declare robot class:
+    HardwareDriveBot robot = new HardwareDriveBot();
+
+    @Override
+    public void runOpMode() {
+
+        // initialize the robot:
+        robot.init(hardwareMap);
+
+        // TODO: on Wednesday, create function in HW class to convert inches to ticks
+        // move the robot by 24 inches:
+        double inches = 24;
+        double rotations = inches / (Math.PI * HardwareDriveBot.WHEEL_DIAMETER);
+        int encoderTarget = (int) (rotations *HardwareDriveBot.ENCODER_ROTATION_40);
+
+        telemetry.addData("Encoder Value", robot.leftMotor.getCurrentPosition());
+        telemetry.addData("Encoder Target", encoderTarget);
+        telemetry.update();
+
+        // wait for the user to press the START button on the DS phone:
+        waitForStart();
+
+        //----------------------------------------
+        // move the robot by a given distance:
+        //----------------------------------------
+
+        // TODO: on Wednesday, create function in HW class to move robot
+        // turn the 2 motors on:
+        robot.start(0.3);
+
+        // This code was used for demo purposes, see below for more compact version
+        //        int pos;
+        //        do {
+        //            pos = robot.leftMotor.getCurrentPosition();
+        //            telemetry.addData("Encoder", pos);
+        //            telemetry.update();
+        //        }
+        //        while( pos < encoderTarget );
+
+        // wait until the target position has been reached:
+        while (robot.leftMotor.getCurrentPosition() < encoderTarget) {
+            idle();   // hand control to Android for a short period of time
+        }
+
+        // turn the 2 motors off:
+        robot.stop();
+
+        //----------------------------------------
+        // turn the robot by a given angle:
+        //----------------------------------------
+
+        // TODO: code does not work, need to reset the encoders first, add function to HW class
+
+        double angle = 90.0;
+        // TODO: WARNING: NO MAGIC NUMBERS, instead, do the math
+        encoderTarget = (int) (7.8 * angle);
+
+        // turn the 2 motors on:
+        robot.spin(0.3);
+
+        // wait until the target position has been reached:
+        while (robot.rightMotor.getCurrentPosition() < encoderTarget) {
+            idle();
+        }
+
+        // turn the 2 motors off:
+        robot.stop();
+
+        // communicate with the user:
+        // telemetry goes to the driver station phone
+        telemetry.addData("Encoder", robot.leftMotor.getCurrentPosition());
+        telemetry.update();
+
+        // send messages to the logcat window (Android Monitor):
+        System.out.println("Encoder (left) = " + robot.leftMotor.getCurrentPosition());
+        System.out.println("Encoder (right) = " + robot.rightMotor.getCurrentPosition());
+
+        // Send various and flexible messages to the logcat window (Android Monitor):
+        // TODO: introduce tags at top of each program
+        Log.i("ROBOT", "Encoder (left) = " + robot.leftMotor.getCurrentPosition());
+
+        sleep(5000);
+    }
+}
